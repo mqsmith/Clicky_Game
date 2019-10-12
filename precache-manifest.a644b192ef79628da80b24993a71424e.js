560e179e424ecf903fb42c86dc25a24661cb49ee
Mike Smith <smith.mike@mac.com>
2019-10-12 12:50:01 -0400

    Updates

---
+++ precache-manifest.a644b192ef79628da80b24993a71424e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0051cc61839675a37cee5d74d6998314",
+    "url": "/Clicky_Game/index.html"
+  },
+  {
+    "revision": "41bee55d0e994bf9117e",
+    "url": "/Clicky_Game/static/css/main.9444760d.chunk.css"
+  },
+  {
+    "revision": "e1a74aee1020e4cbcbc3",
+    "url": "/Clicky_Game/static/js/2.bf74cbd3.chunk.js"
+  },
+  {
+    "revision": "41bee55d0e994bf9117e",
+    "url": "/Clicky_Game/static/js/main.849e1c01.chunk.js"
+  },
+  {
+    "revision": "d6b21d048d540954b6ac",
+    "url": "/Clicky_Game/static/js/runtime-main.0274f1ec.js"
+  }
+]);
